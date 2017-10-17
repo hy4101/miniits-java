506fa91e5265974def2e62c6a53a503e6e07d766
hy4101 <857454370@qq.com>
2017-10-17 20:41:13 +0800

    miniits java 8

---
+++ miniits-java-8/src/main/java/com/java8/Stat.java
@@ -0,0 +1,61 @@
+package com.java8;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * @author: wq
+ * @Date: 2017/10/17
+ * @Time: 20:25
+ * <p>
+ * Description:
+ * ***
+ */
+
+public class Stat {
+
+    @Test
+    public void miniitsInit() {
+        List<String> names1 = new ArrayList<String>();
+        names1.add("31");
+        names1.add("2");
+        names1.add("23");
+        names1.add("4");
+        names1.add("6");
+
+        List<String> names2 = new ArrayList<String>();
+        names2.add("31");
+        names2.add("2");
+        names2.add("23");
+        names2.add("4");
+        names2.add("6");
+
+        System.out.println("使用 Java 7 语法: ");
+
+        sortUsingJava7(names1);
+        System.out.println(names1);
+        System.out.println("使用 Java 8 语法: ");
+
+        sortUsingJava8(names2);
+        System.out.println(names2);
+    }
+
+    // 使用 java 7 排序
+    private void sortUsingJava7(List<String> names){
+        Collections.sort(names, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                return s1.compareTo(s2);
+            }
+        });
+    }
+
+    // 使用 java 8 排序
+    private void sortUsingJava8(List<String> names){
+        Collections.sort(names, (s1, s2) -> s1.compareTo(s2));
+    }
+}
