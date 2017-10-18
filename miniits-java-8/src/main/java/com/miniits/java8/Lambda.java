c285011497f35b5cbbf203e4a3482cb1645c37ac
hy4101 <857454370@qq.com>
2017-10-18 11:52:13 +0800

    miniits java 8

--- miniits-java-8/src/main/java/com/miniits/java8/Lambda.java
+++ miniits-java-8/src/main/java/com/miniits/java8/Lambda.java
@@ -12,64 +12,31 @@ import org.junit.Test;
  */
 public class Lambda {
     @Test
-    public void main() {
+    public void LambdaTest() {
 
-//        Lambda tester = new Lambda();
-        // 类型声明
-        MathOperation addition = (int a, int b) -> a + b;
-
-        // 不用类型声明
-        MathOperation subtraction = (a, b) -> a - b;
-
-        // 大括号中的返回语句
-        MathOperation multiplication = (int a, int b) -> {
+        // 类型声明-不带花括号
+        ILambda addition = (int a, int b) -> a + b;
+        // 不用类型声明-不带-不带花括号
+        ILambda subtraction = (a, b) -> a - b;
+        // -不带花括号中的返回语句
+        ILambda multiplication = (int a, int b) -> {
             return a * b;
         };
+        // 没有-不带花括号及返回语句
+        ILambda division = (int a, int b) -> a / b;
 
-        // 没有大括号及返回语句
-        MathOperation division = (int a, int b) -> a / b;
-
-        System.out.println("10 + 5 = " + operate(10, 5, addition));
-        System.out.println("10 - 5 = " + operate(10, 5, subtraction));
-        System.out.println("10 x 5 = " + operate(10, 5, multiplication));
-        System.out.println("10 / 5 = " + operate(10, 5, division));
-
-        // 不用括号
-        GreetingService greetService1 = message ->
-                System.out.println("Hello " + message);
-
-        // 用括号
-        GreetingService greetService2 = (message) ->
-                System.out.println("Hello " + message);
-
-        greetService1.sayMessage("Runoob");
-        greetService2.sayMessage("Google");
+        System.out.println(fun(1, 2, addition));
+        System.out.println(fun(1, 2, subtraction));
+        System.out.println(fun(1, 2, multiplication));
+        System.out.println(fun(1, 2, division));
     }
 
-    interface MathOperation {
-        int operation(int a, int b);
+    interface ILambda {
+        int lm(int a, int b);
     }
 
-    interface GreetingService {
-        void sayMessage(String message);
+    private int fun(int a, int b, ILambda mathOperation) {
+        return mathOperation.lm(a, b);
     }
 
-    private int operate(int a, int b, MathOperation mathOperation) {
-        return mathOperation.operation(a, b);
-    }
-
-
-
-
-    @Test
-    public void aa() {
-        T t = (int a, int q) -> a + q;
-        T t1 = (int a, int q) -> a * q;
-        System.out.println(t.tt(1, 3));
-        System.out.println(t1.tt(1, 3));
-    }
-
-    interface T {
-        int tt(int q, int w);
-    }
 }
